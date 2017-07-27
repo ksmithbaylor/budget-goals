33e87e8d46f82f325b2624aa4565a2861df8dbef
Kevin Smith <ksmithbaylor@gmail.com>
2017-07-27 12:36:00 -0400

    Add nice tooltips to chart

---
+++ src/components/Summary/index.js
@@ -0,0 +1,90 @@
+// @flow
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { zipWith } from 'lodash';
+import {
+  VictoryChart,
+  VictoryAxis,
+  VictoryStack,
+  VictoryBar,
+  VictoryTooltip,
+  VictoryTheme,
+  Line
+} from 'victory';
+
+import { monthRange, chartColors } from '../../config';
+import monthlyGoalData, { type MonthlyGoalData } from '../../store/selectors/monthlyGoalData';
+import Money from '../../model/money';
+import './tooltip';
+
+function Summary(props: { data: MonthlyGoalData }) {
+  const bars = props.data.map((goalData, i) =>
+    <VictoryBar
+      key={goalData.goal.id}
+      style={{ data: { fill: chartColors[i] } }}
+      data={zipWith(monthRange, goalData.data, (month, amount) => {
+        return {
+          x: month.name,
+          y: amount ? parseFloat(amount.toFixed(2)) : 0.0
+        };
+      })}
+      events={[
+        {
+          target: 'data',
+          eventHandlers: {
+            onMouseOver() {
+              window.tooltipText = goalData.goal.name;
+            },
+            onMouseOut() {
+              window.tooltipText = '';
+            }
+          }
+        }
+      ]}
+    />
+  );
+
+  const sums = monthRange.map((month, i) =>
+    new Money(
+      props.data
+        .map(goalData => (goalData.data ? goalData.data[i] : null))
+        .reduce((a, b) => a.plus(b || new Money(0)), new Money(0))
+    ).toString()
+  );
+
+  return (
+    <div onMouseMove={window.onSummaryMouseMove}>
+      <VictoryChart
+        theme={VictoryTheme.material}
+        height={500}
+        width={1000}
+        padding={75}
+        domainPadding={{ x: 30, y: 20 }}
+      >
+        <VictoryStack
+          labels={sums}
+          style={{ labels: { fontSize: '12px', fontWeight: 'bold' } }}
+        >
+          {bars}
+        </VictoryStack>
+        <VictoryAxis
+          tickValues={monthRange.map(month => month.toString().substr(0, 3))}
+          gridComponent={<Line style={{ display: 'none' }} />}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickCount={10}
+          tickFormat={tick => new Money(tick).toString()}
+          gridComponent={<Line style={{ display: 'none' }} />}
+        />
+      </VictoryChart>
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({
+  data: monthlyGoalData(state)
+});
+
+export default connect(mapStateToProps)(Summary);
---
+++ src/components/Summary/tooltip.js
@@ -0,0 +1,34 @@
+// @flow
+
+import { injectGlobal } from 'styled-components';
+
+window.tooltipText = '';
+window.tooltip = null;
+
+injectGlobal`
+  .goalTooltip {
+    position: absolute;
+    font-size: 16px;
+    background-color: white;
+    padding: 0.5em;
+    box-shadow: 0px 0px 55px -12px rgba(0,0,0,1);
+  }
+`;
+
+window.onSummaryMouseMove = function onSummaryMouseMove(event) {
+  if (!window.tooltip) {
+    window.tooltip = document.createElement('div');
+    window.tooltip.classList.add('goalTooltip');
+    document.body.appendChild(window.tooltip);
+  }
+
+  window.tooltip.style.top = event.pageY + 20 + 'px';
+  window.tooltip.style.left = event.pageX + 20 + 'px';
+  window.tooltip.textContent = window.tooltipText;
+
+  if (window.tooltipText) {
+    window.tooltip.style.visibility = 'initial';
+  } else {
+    window.tooltip.style.visibility = 'hidden';
+  }
+};
